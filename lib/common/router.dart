51bf9ef48ac4fd01ac13cac207ac930777e60bc9
Abdelrahman Tarek Elabrak <abdelrahmantelabrak@gmail.com>
2024-05-07 06:48:40 +0300

    add quiz page

--- lib/common/router.dart
+++ lib/common/router.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quizlet_clone/view/intro/intro_page.dart';
+import 'package:quizlet_clone/view/quiz/quiz_page.dart';
 import 'package:quizlet_clone/view/splash/splash_screen.dart';
 
 import '../view/home/home_page.dart';
@@ -11,12 +12,14 @@ class AppRouter {
         return MaterialPageRoute(builder: (_) => const SplashScreen());
       case '/intro':
         return MaterialPageRoute(builder: (_) => const IntroPage());
-      case '/home':
+      case 'quiz_page':
       // Extracting arguments from settings
-        final args = settings.arguments as Map<String, dynamic>;
+        final args = settings.arguments as String;
         return MaterialPageRoute(
-          builder: (_) => HomePage(accountData: args['accountData']),
+          builder: (_) => QuizPage(quizId: args),
         );
+      case '/home':
+        return MaterialPageRoute(builder: (_) => const HomePage());
       default:
       // If route not found, navigate to intro page
         return null;
